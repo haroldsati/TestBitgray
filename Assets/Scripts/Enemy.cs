4b7a92594cdcc232d3d578cb6b80d8cf24f1a887
Harold Satizabal <haroldsati@gmail.com>
2016-07-26 15:10:10 -0500

    Import Vuforia And Implement simple enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Enemy : MonoBehaviour
+{
+    private event Action Killed;
+    private NavMeshAgent agent;
+    private Collider myCollider;
+
+    public void Initialize(Vector3 target, Action killed)
+    {
+        agent = GetComponent<NavMeshAgent>();
+        myCollider = GetComponent<Collider>();
+
+        Killed = killed;
+        SetTarget(target);
+    }
+
+    public void SetParent(Transform parent, Vector3 position)
+    {
+        transform.SetParent(parent);
+        transform.position = position;
+    }
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (myCollider.Raycast(ray, out hit, 100.0F))
+                RaiseKilled();
+        }
+    }
+
+    private void RaiseKilled()
+    {
+        if (Killed != null)
+            Killed();
+
+        agent.Stop();
+        Destroy(gameObject);
+    }
+
+    public void SetTarget(Vector3 targetPosition)
+    {
+        agent.SetDestination(targetPosition);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Target")
+            agent.Stop();
+    }
+
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Transform enemiesParent;
+    [SerializeField]
+    private List<GameObject> enemies;
+    [SerializeField]
+    private Vector2 minRangeToDeploy;
+    [SerializeField]
+    private Vector2 maxRangeToDeploy;
+    [SerializeField]
+    private int initialAmountOfEnemies;
+
+	public void Start ()
+    {
+        InstantieEnemies(initialAmountOfEnemies);
+    }
+
+    private void InstantieEnemies(int amountOfEnemies)
+    {
+        for (int i = 0; i < amountOfEnemies; i++)
+            InstantiateEnemy();
+    }
+
+    private void InstantiateEnemy()
+    {
+        int index = Random.Range(0, enemies.Count - 1);
+        GameObject instance = Instantiate(enemies[index]);
+        Enemy enemy = instance.GetComponent<Enemy>();
+        enemy.SetParent(enemiesParent, GetInitialPosition());
+        enemy.Initialize(target.position, OnEnemyKilled);
+    }
+
+    private Vector3 GetInitialPosition()
+    {
+        Vector3 position = Vector3.zero;
+
+        if (GetRandomBool())
+            position = SetUpXFirst(position);
+        else
+            position = SetUpZFirst(position);
+
+        return position;
+    }
+
+    private Vector3 SetUpXFirst(Vector3 position)
+    {
+        position.x = Random.Range(minRangeToDeploy.x, maxRangeToDeploy.x);
+        position.x *= GetRandomBool() ? 1 : -1;
+        position.z = Random.Range(-maxRangeToDeploy.y, maxRangeToDeploy.y);
+        return position;
+    }
+
+    private Vector3 SetUpZFirst(Vector3 position)
+    {
+        position.z = Random.Range(minRangeToDeploy.y, maxRangeToDeploy.y);
+        position.z *= GetRandomBool() ? 1 : -1;
+        position.x = Random.Range(-maxRangeToDeploy.x, maxRangeToDeploy.x);
+        return position;
+    }
+
+    private bool GetRandomBool()
+    {
+        return Random.Range(0, 100) < 50 ? false : true;
+    }
+	
+    private void OnEnemyKilled()
+    {
+        InstantieEnemies(2);
+    }
+}
