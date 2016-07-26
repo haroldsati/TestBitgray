45a1d535c4ea35facb511f0871ff108c6f38deed
Harold Satizabal <haroldsati@gmail.com>
2016-07-26 12:12:12 -0500

    WIP User Loader

---
+++ Assets/Scripts/UserLoader.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+
+public class UserLoader : MonoBehaviour
+{
+    public event System.Action<UserData> UserLoaded;
+
+    public void LoadUser(System.Action<UserData> userLoaded)
+    {
+        Debug.Log("LoadUser");
+        UserLoaded = userLoaded;
+        StartCoroutine(MakeRequest());
+    }
+
+    private IEnumerator MakeRequest()
+    {
+        WWW request;
+        yield return request = new WWW("http://jsonplaceholder.typicode.com/users");
+
+        if (request != null)
+            LoadUser(request.text);
+        else
+            RaiseUserLoaded(null);
+    }
+
+    private void LoadUser(string json)
+    {
+        JsonData data = JsonMapper.ToObject(json);
+        int index = Random.Range(0, data.Count);
+        UserData user = JsonMapper.ToObject<UserData>(data[index].ToJson());
+        RaiseUserLoaded(user);
+    }
+
+    private void RaiseUserLoaded(UserData data)
+    {
+        if (UserLoaded != null)
+            UserLoaded(data);
+
+        UserLoaded = null;
+    }
+}
