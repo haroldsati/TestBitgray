e7a57281524ad0e5d8efce6f26c3209f58988659
Harold Satizabal <haroldsati@gmail.com>
2016-07-28 11:42:13 -0500

    Visual Ajustments

---
+++ Assets/Scripts/Gameplay/WaterScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Gameplay
+{
+    public class WaterScroll : MonoBehaviour
+    {
+        [SerializeField]
+        private float scrollSpeed = 0.1f;
+        [SerializeField]
+        private MeshRenderer myRenderer;
+
+        private float offset = 0;
+
+        private void Update()
+        {
+            offset += Time.deltaTime * scrollSpeed;
+            myRenderer.sharedMaterial.SetTextureOffset("_MainTex", new Vector2(0, offset));
+        }
+    }
+}
